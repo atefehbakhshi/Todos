fc0ef91b245c008e78fc92b608f1f0939b900e66
Atefeh <at.bakhshi1373@gmail.com>
2022-12-12 07:20:48 +0330

    feat:Home page UI

---
+++ script.js
@@ -0,0 +1,14 @@
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const form = document.querySelector("#form");
+openMenu.addEventListener("click", () => {
+  hamburgerMenu.style.transform = "translateX(0)";
+  form.style.opacity = 0;
+  openMenu.style.opacity = 0;
+});
+closeMenu.addEventListener("click", () => {
+  hamburgerMenu.style.transform = "translateX(100%)";
+  form.style.opacity = 1;
+  openMenu.style.opacity = 1;
+});
