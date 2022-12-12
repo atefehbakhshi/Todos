bf966db6d92dd429090351763c379f4aba905104
Atefeh <at.bakhshi1373@gmail.com>
2022-12-12 17:24:20 +0330

    feat: create new item and send it to server

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const openMenu = document.querySelector("#open-menu");
-const closeMenu = document.querySelector("#close-menu");
-const hamburgerMenu = document.querySelector("#hamburger-menu");
-const form = document.querySelector("#form");
-openMenu.addEventListener("click", () => {
-  hamburgerMenu.style.transform = "translateX(0)";
-  openMenu.style.opacity = 0;
-});
-closeMenu.addEventListener("click", () => {
-  hamburgerMenu.style.transform = "translateX(100%)";
-  openMenu.style.opacity = 1;
-});
+// API
+// const API_URL = "https://63969f1c90ac47c68089df44.mockapi.io";
+const listForm = document.querySelector("#create-list");
+
+// Create
+async function createNewList(e) {
+  e.preventDefault();
+  const newList = gatherFormDate(e);
+  try {
+    const res = await fetch(
+      `https://63969f1c90ac47c68089df44.mockapi.io/todos`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newList),
+      }
+    );
+    successfullMessage();
+    resetForm();
+  } catch (error) {
+    console.log(error);
+  }
+}
+function gatherFormDate(e) {
+  const { title, description, dueDate } = e.target;
+  console.log();
+  return {
+    title: title.value,
+    dueDate: dueDate.value,
+    description: description.value,
+    createdAt: new Date().getTime(),
+    updatedAt: new Date().getTime(),
+    checked: false,
+    id: new Date().getTime(),
+  };
+}
+listForm.addEventListener("submit", createNewList);
+
+// Successful message
+function successfullMessage() {
+  const message = document.querySelector(".toast");
+  message.style.transform = "translateX(0)";
+  setTimeout(() => {
+    message.style.transform = "translateX(-150%)";
+  }, 3000);
+}
+// Reset form
+function resetForm(){
+  listForm.reset();
+}
