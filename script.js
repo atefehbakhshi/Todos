36203259f0e3588b3c0910130f5d473f00bc29f4
Atefeh <at.bakhshi1373@gmail.com>
2022-12-12 19:03:32 +0330

    feat: required inputs when submitting

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-// API
-// const API_URL = "https://63969f1c90ac47c68089df44.mockapi.io";
+const API_URL = "https://63969f1c90ac47c68089df44.mockapi.io";
+
 const listForm = document.querySelector("#create-list");
+
+const titleLabel = document.querySelector("#title-label");
+const titleInput = document.querySelector("#title");
+const titleAlert = document.querySelector("#required-title");
+
+const dueDateLabel = document.querySelector("#dueDate-label");
+const dueDateInput = document.querySelector("#dueDate");
+const dueDateAlert = document.querySelector("#required-dueDate");
 
 // Create
 async function createNewList(e) {
   e.preventDefault();
   const newList = gatherFormDate(e);
-  try {
-    const res = await fetch(
-      `https://63969f1c90ac47c68089df44.mockapi.io/todos`,
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newList),
-      }
-    );
-    successfullMessage();
-    resetForm();
-  } catch (error) {
-    console.log(error);
+  if (newList) {
+    try {
+      const res = await fetch(
+        `https://63969f1c90ac47c68089df44.mockapi.io/todos`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(newList),
+        }
+      );
+      removeAlert();
+      successfullMessage();
+      resetForm();
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 function gatherFormDate(e) {
   const { title, description, dueDate } = e.target;
-  console.log();
-  return {
-    title: title.value,
-    dueDate: dueDate.value,
-    description: description.value,
-    createdAt: new Date().getTime(),
-    updatedAt: new Date().getTime(),
-    checked: false,
-    id: new Date().getTime(),
-  };
+  if (title.value === "") {
+    titleLabel.style.color = "red";
+    titleInput.style.border = "3px solid red";
+    titleAlert.style.display = "block";
+  } else if (dueDate.value === "") {
+    dueDateLabel.style.color = "red";
+    dueDateInput.style.border = "3px solid red";
+    dueDateAlert.style.display = "block";
+  } else {
+    return {
+      title: title.value,
+      dueDate: dueDate.value,
+      description: description.value,
+      createdAt: new Date().getTime(),
+      updatedAt: new Date().getTime(),
+      checked: false,
+      id: new Date().getTime(),
+    };
+  }
 }
 listForm.addEventListener("submit", createNewList);
 
@@ -45,6 +65,15 @@ function successfullMessage() {
   }, 3000);
 }
 // Reset form
-function resetForm(){
+function resetForm() {
   listForm.reset();
+}
+// Remove Alert
+function removeAlert() {
+  titleLabel.style.color = "white";
+  titleInput.style.border = "none";
+  titleAlert.style.display = "none";
+  dueDateLabel.style.color = "white";
+  dueDateInput.style.border = "none";
+  dueDateAlert.style.display = "none";
 }
