5a175498c2eeb0e343467ad002cace6ae8b1262c
Atefeh <at.bakhshi1373@gmail.com>
2022-12-13 07:10:53 +0330

    style: when hamburger menu opened, the pagination nav will send to back

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,11 +1,16 @@
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+const paginationNav  = document.querySelector("#pagination-nav");
+
 openMenu.addEventListener("click", () => {
   hamburgerMenu.style.transform = "translateX(0)";
   openMenu.style.opacity = 0;
+  paginationNav.style.zIndex=-1;
 });
 closeMenu.addEventListener("click", () => {
   hamburgerMenu.style.transform = "translateX(100%)";
   openMenu.style.opacity = 1;
+  paginationNav.style.zIndex=1;
 });
